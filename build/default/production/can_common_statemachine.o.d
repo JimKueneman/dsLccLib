31d412ab09d7d271538207890d3dbd94397ac981
Jim Kueneman <JimKueneman@yahoo.com>
2024-11-07 15:38:48 -0700

    missing files

---
+++ build/default/production/can_common_statemachine.o.d
@@ -0,0 +1,13 @@
+build/default/production/can_common_statemachine.o: \
+ can_common_statemachine.c node.h openlcb_buffers.h can_buffers.h \
+ openlcb_utilities.h openlcb_defines.h
+
+node.h:
+
+openlcb_buffers.h:
+
+can_buffers.h:
+
+openlcb_utilities.h:
+
+openlcb_defines.h:
---
+++ build/default/production/can_outgoing_statemachine.o.d
@@ -0,0 +1,22 @@
+build/default/production/can_outgoing_statemachine.o: \
+ can_outgoing_statemachine.c openlcb_buffers.h mcu_driver.h \
+ can_outgoing_statemachine.h can_buffers.h can_common_statemachine.h \
+ openlcb_defines.h debug.h node.h openlcb_utilities.h
+
+openlcb_buffers.h:
+
+mcu_driver.h:
+
+can_outgoing_statemachine.h:
+
+can_buffers.h:
+
+can_common_statemachine.h:
+
+openlcb_defines.h:
+
+debug.h:
+
+node.h:
+
+openlcb_utilities.h:
